8f44bfa2ed707d84efd2d28bd3025e2f035a8ee4
amirhossein <amirhosseinhphpam@gmail.com>
2023-07-03 19:43:59 +0330

    fix menu in mobile

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -12,6 +12,10 @@ import MainSocial from '../shared/Social/MainSocial';
 
 const Menu = () => {
     const [menuOpen, setMenuOpen] = useState(false)
+    const menuHandler = () => {
+        setMenuOpen(!menuOpen)
+    }
+    
     return (
         <>
             <Social />
@@ -22,25 +26,25 @@ const Menu = () => {
                         amirhossein
                     </div>
                     <ul className={`flex flex-col md:flex-row md:items-center py-20 px-10 md:px-0 md:py-0 h-screen md:h-auto absolute md:static left-0 w-full md:w-auto transition-all duration-500 ease-in-out bg-background md:bg-transparent ${menuOpen ? 'top-0' : 'top-[-900px]'}`}>
-                        <NavLink to='/' className={(link) => link.isActive ? 'text-white font-fira-500' : ''}>
+                        <NavLink onClick={menuHandler} to='/' className={(link) => link.isActive ? 'text-white font-fira-500' : ''}>
                             <li className='mr-8 hover:text-white transition-all'>
                                 <span className='text-primary'>#</span>
                                 home
                             </li>
                         </NavLink>
-                        <NavLink to='/projects' className={(link) => link.isActive ? 'text-white font-fira-500' : ''}>
+                        <NavLink onClick={menuHandler} to='/projects' className={(link) => link.isActive ? 'text-white font-fira-500' : ''}>
                             <li className='mr-8 mt-4 md:mt-0 hover:text-white transition-all'>
                                 <span className='text-primary'>#</span>
                                 projects
                             </li>
                         </NavLink>
-                        <NavLink to='/about' className={(link) => link.isActive ? 'text-white font-fira-500' : ''}>
+                        <NavLink onClick={menuHandler} to='/about' className={(link) => link.isActive ? 'text-white font-fira-500' : ''}>
                             <li className='mr-8 mt-4 md:mt-0 hover:text-white transition-all'>
                                 <span className='text-primary'>#</span>
                                 about-me
                             </li>
                         </NavLink>
-                        <NavLink to='/contacts' className={(link) => link.isActive ? 'text-white font-fira-500' : ''}>
+                        <NavLink onClick={menuHandler} to='/contacts' className={(link) => link.isActive ? 'text-white font-fira-500' : ''}>
                             <li className='mr-8 mt-4 md:mt-0 hover:text-white transition-all'>
                                 <span className='text-primary'>#</span>
                                 contacts
@@ -55,7 +59,7 @@ const Menu = () => {
                         </div>
                     </ul>
                     <div className='md:hidden z-10'>
-                        <img className='cursor-pointer' onClick={() => setMenuOpen(!menuOpen)} src={menuOpen ? `${close}` : `${open}`} alt='menu' />
+                        <img className='cursor-pointer' onClick={menuHandler} src={menuOpen ? `${close}` : `${open}`} alt='menu' />
                     </div>
                 </div>
             </nav>
