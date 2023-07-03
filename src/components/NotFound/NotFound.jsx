7727800f54ac6e00031d636c9bc53795d97e3579
amirhossein <amirhosseinhphpam@gmail.com>
2023-07-04 00:30:10 +0330

    completed page not found

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+
+// shared
+import HeadPage from '../shared/HeadPage/HeadPage';
 
 const NotFound = () => {
     return (
-        <div>
-            NotFound
-        </div>
+        <section className='container mt-12 h-64'>
+            <Link to='/'>
+                <HeadPage pageName='notfound' descPage='sorry, the page not found' />
+            </Link>
+        </section>
     );
 };
 
