a29e2582827a9f9136fe45b21681c67046ef3ee6
amirhossein <amirhosseinhphpam@gmail.com>
2023-07-13 14:06:43 +0330

    add skill project form

--- src/database/database.js
+++ src/database/database.js
@@ -10,7 +10,7 @@ const projects = [
     { id: 2, img: restaurant, using: 'Javascript React HTML5 TailwindCss ReactScroll', nameProject: 'Restaurant', desc: 'Café Restaurant - Reservation and Menu Display Functionality', toLink: 'https://restaurant-landing-bice.vercel.app/' },
     { id: 3, img: smartshop, using: 'Javascript React HTML5 CSS3 Context SwiperJS', nameProject: 'Smart Shop', desc: 'Smartwatch Store - Product Addition and Deletion Capabilities', toLink: 'https://smart-shop-gamma.vercel.app/' },
     { id: 4, img: portfolio, using: 'Javascript ReactJS HTML5 ReactRouterDom TailwindCss', nameProject: 'Portfolio', desc: 'Personal Website for Contacting Me and Showcasing Projects and Skills', toLink: 'https://portfolio-gilt-five-39.vercel.app/' },
-    { id: 5, img: form, using: 'Javascript ReactJS HTML5 Axios Formik Yup ReactRouterDom', nameProject: 'Form', desc: 'Validated Form for User Registration, Account Deletion, and User Profile Update', toLink: 'https://form-three-xi.vercel.app/' },
+    { id: 5, img: form, using: 'Javascript ReactJS HTML5 Axios Formik Yup ReactRouterDom TailwindCss', nameProject: 'Form', desc: 'Validated Form for User Registration, Account Deletion, and User Profile Update', toLink: 'https://form-three-xi.vercel.app/' },
 ]
 
 const skills = [
